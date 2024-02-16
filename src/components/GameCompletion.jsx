f2fcb27b98cec3f86d3e6fa2dea7abba49797b73
Shin Kim <shin.kim8245@gmail.com>
2024-02-16 09:15:26 -0500

    added sound effect for completion

--- src/components/GameCompletion.jsx
+++ src/components/GameCompletion.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import { CardDataContext } from "../context/CardDataContext";
 import Confetti from "react-confetti";
@@ -11,16 +11,21 @@ const GameCompletion = () => {
 
   const time = `${diffHours}h ${diffMinutes}m ${diffSeconds}s`;
 
+  useEffect(() => {
+    const audio = new Audio("/sounds/finish.wav");
+    audio.play();
+  }, []);
+
   return (
     <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
       <Typography variant="h3">🎉</Typography>
-      <Typography color="white">You've done it</Typography>
+      <Typography color="white">Well done!</Typography>
       <Box>
         <Typography color="white">Time: {time}</Typography>
         <Typography color="white">Moves: {moves}</Typography>
       </Box>
       <Confetti
-        numberOfPieces={2000} // You can adjust the number of confetti pieces
+        numberOfPieces={5000} // You can adjust the number of confetti pieces
         recycle={false}
         gravity={0.1}
         width={window.innerWidth}
