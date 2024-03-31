a271e2782975464a169970def5157eae7bad727b
Shin Kim <shin.kim8245@gmail.com>
2024-03-30 23:03:43 -0400

    AWS URL

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,4 +1,4 @@
 const ASSETS_URL =
-  "https://shinkim-public.s3.us-east-2.amazonaws.com/flipmatch-assets";
+  "https://shinkim-public.s3.us-east-2.amazonaws.com/flipmatch-assets/";
 
 export { ASSETS_URL };
